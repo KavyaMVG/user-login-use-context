7cf75748955993f91369330bd42ff38c02717f46
kavyaMVG <kavya.mvg@gmail.com>
2023-01-31 14:26:17 +0530

    Initial commit

---
+++ src/components/MainHeader/Navigation.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useContext } from "react";
+import AuthContext from "../../store/AuthContext";
+
+import classes from "./Navigation.module.css";
+
+const Navigation = (props) => {
+  const context = useContext(AuthContext);
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {context.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
